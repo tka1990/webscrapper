f14896f44d4bd774411935e3723150f7232643a2
tka1990 <thinesh.kumar@live.com>
2025-08-10 20:04:55 +0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-
-from __future__ import annotations
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-app = FastAPI()
-@app.get("/healthz", response_class=PlainTextResponse)
-async def healthz()->str: return "ok"
